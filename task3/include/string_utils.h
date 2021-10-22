0e9b11c07a403f4d59e3fe0680d720e6ab1fce9a
Mikael Escolin <mikaelescolin@gmail.com>
2021-10-22 11:13:23 +0100

    Separate string functions from main

---
+++ task3/include/string_utils.h
@@ -0,0 +1,10 @@
+#ifndef STRING_UTILS
+
+#define STRING_UTILS
+
+int find_index(char *, int, char *, int);
+int get_end_of_string_index(char *);
+char *get_substring(char *, int, int);
+int is_strings_equal(char *, char *);
+
+#endif
--- task3/src/main.c
+++ task3/src/main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include "string_utils.h"
 
-int find_index(char *, int, char *, int);
 char *get_column(char *, int, int, int);
 int get_column_name_index(char *, int, char *, int);
 int get_column_start(char *, int, int);
 int get_end_of_string_index(char *);
 int get_pid_for_command(FILE *, char *);
-char *get_substring(char *, int, int);
-int is_strings_equal(char *, char *);
 void list_processes();
 
 int main(int argc, char *argv[])
@@ -42,24 +40,6 @@ int main(int argc, char *argv[])
   }
 
   return 0;
-}
-
-int find_index(char *string, int string_length, char *substring, int substring_length)
-{
-  int substring_index = -1;
-  for (int i = 0; i < string_length - 1; i++)
-  {
-    char *curr_substring = get_substring(string, i, i + substring_length - 1);
-    int is_equal = is_strings_equal(substring, curr_substring);
-
-    if (is_equal == 1)
-    {
-      substring_index = i;
-      return substring_index;
-    }
-  }
-
-  return substring_index;
 }
 
 char *get_column(char *row, int max_row_length, int column_start, int column_end)
@@ -102,21 +82,6 @@ int get_column_start(char *line, int line_max_length, int end)
   return end;
 }
 
-int get_end_of_string_index(char *string)
-{
-  int i;
-  for (i = 0; *(string + i) != '\0'; i++)
-  {
-    char character = *(string + i);
-    if ((character >= 0 && character <= 31) || character == 127)
-    {
-      return i;
-    }
-  }
-
-  return i;
-}
-
 int get_pid_for_command(FILE *processes, char *command)
 {
   char *row = NULL;
@@ -140,45 +105,6 @@ int get_pid_for_command(FILE *processes, char *command)
       return atoi(pid);
     }
   } while (read != -1);
-
-  return -1;
-}
-
-char *get_substring(char *string, int start, int end)
-{
-  int end_index;
-  if (end == -1)
-  {
-    end_index = (int)get_end_of_string_index(string);
-  }
-  else
-  {
-    end_index = end;
-  }
-
-  size_t length = (end_index - start);
-  char *substring = (char *)malloc(length + 1);
-  memcpy(substring, (string + start), length);
-  *(substring + length) = '\0';
-
-  return substring;
-}
-
-int is_strings_equal(char *string1, char *string2)
-{
-  int i;
-  for (i = 0; *(string1 + i) && *(string2 + i); i++)
-  {
-    if (*(string1 + i) != *(string2 + i))
-    {
-      return -1;
-    }
-  }
-
-  if (*(string1 + i) == '\0' && *(string2 + i) == '\0')
-  {
-    return 1;
-  }
 
   return -1;
 }
